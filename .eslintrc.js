329caec4b68920f9c562e9fe30509173a68901a2
Ray Hernandez <rayhernandez014@gmail.com>
2022-07-06 15:47:22 -0400

    last modifications

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,36 @@
+/* eslint-env node */
 module.exports = {
   'env': {
-    'commonjs': true,
-    'es2021': true,
-    'node': true,
-    'jest': true
+    'browser': true,
+    'es6': true,
+    'jest/globals': true,
+    "cypress/globals": true
   },
-  'extends': 'eslint:recommended',
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier'
+  ],
   'parserOptions': {
-    'ecmaVersion': 'latest'
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2020,
+    'sourceType': 'module'
   },
+  'plugins': [
+    "react", "jest", "cypress"
+  ],
   'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
     'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
-    'no-console': 0
+    'no-console': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+    "no-unused-vars": "warn"
+  },
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
   }
 }
--- controllers/users.js
+++ controllers/users.js
@@ -5,13 +5,14 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
-  if (!password){
+  if (!password) {
     return response.status(400).json({
-      error: 'password is missing' })
-  }
-  else if (password.length < 3){
+      error: 'password is missing'
+    })
+  } else if (password.length < 3) {
     return response.status(400).json({
-      error: 'password is too short' })
+      error: 'password is too short'
+    })
   }
 
   const existingUser = await User.findOne({ username })
@@ -25,17 +26,21 @@ usersRouter.post('/', async (request, response) => {
   const user = new User({
     username,
     name,
-    passwordHash,
+    passwordHash
   })
 
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
-
 })
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 })
+  const users = await User.find({}).populate('blogs', {
+    url: 1,
+    title: 1,
+    author: 1,
+    likes: 1
+  })
   response.json(users)
 })
 module.exports = usersRouter
